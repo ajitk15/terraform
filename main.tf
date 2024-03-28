44ad001ada77c7639885f833e0ef5295fe6c9b33
Ajit Kumar P <36964766+ajitk15@users.noreply.github.com>
2024-03-29 00:49:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ resource "aws_iam_user" "admin-user" {
 resource "aws_iam_policy_attachment" "admin-access" {
   name       = file("admin-policy.json")
   users      = aws_iam_user.admin-user.name
-  policy_arn = arn:aws:iam::058264085477:policy/AdminUsers
+  policy_arn = "arn:aws:iam::058264085477:policy/AdminUsers"
 }
 
